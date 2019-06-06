d0e5bdde335fa142a5377467c31b69cc7091206f
Angelet <angelet@abc.com>
2019-06-06 23:29:40 +0200

    Refactored prod code: extract method: isMiltipleOf400

--- leapyear/src/main/java/com/bnppf/kata/leapyear/LeapYear.java
+++ leapyear/src/main/java/com/bnppf/kata/leapyear/LeapYear.java
@@ -3,7 +3,11 @@ package com.bnppf.kata.leapyear;
 public class LeapYear {
 
 	public boolean isLeapYear(int year) {
-		return isMultipleOf4(year) && (isNotMiltipleOf100(year) ||  year%400 == 0);
+		return isMultipleOf4(year) && (isNotMiltipleOf100(year) ||  isMiltipleOf400(year));
+	}
+
+	private boolean isMiltipleOf400(int year) {
+		return year%400 == 0;
 	}
 
 	private boolean isNotMiltipleOf100(int year) {
